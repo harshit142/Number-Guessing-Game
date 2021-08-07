7e3207b0f542829695e510fbbcf6a0e5a6ada4ed
harshit142 <72761621+harshit142@users.noreply.github.com>
2021-08-07 18:07:31 +0530

    Delete PyDict.py

--- PyDict.py
+++
@@ -1,6 +0,0 @@
-dict = { 'List' : "List is a collection which is ordered and changeable. Allows duplicate members."
-, 'Tuple' : "Tuple is a collection which is ordered and unchangeable. Allows duplicate members."
-, 'Set' : "Set is a collection which is unordered and unindexed. No duplicate members."
-, 'Dictionary' : "Dictionary is a collection which is ordered* and changeable. No duplicate members."}
-x = dict[input()]
-print(x)
